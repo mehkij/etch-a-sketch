d830b070db47732d7ccd4066e792359226a3652e
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-09-14 20:36:17 -0400

    Create grid

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World!")
+const gridContainer = document.querySelector("#grid-container");
+
+for (let i = 0; i < 16; i++) {
+  const gridDiv = document.createElement("div");
+  gridDiv.classList.add("grid")
+  gridContainer.appendChild(gridDiv);
+}
