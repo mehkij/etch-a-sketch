c3696c5cc97ef417f1a355d33c584f3642a4aba0
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-09-15 12:39:51 -0400

    Fix 16x16 grid

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const gridContainer = document.querySelector("#grid-container");
+let hovering = false;
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 240; i++) {
   var gridDiv = document.createElement("div");
   gridDiv.classList.add("grid");
-  gridDiv.style.cssText = "height: 31.25px; background-color: red";
+  gridDiv.style.cssText = "width: 31.25px; height: 31.25px";
   gridContainer.appendChild(gridDiv);
 }
 
-// add button that sets the color of the grid to white ("clears the grid")
+// // add button that sets the color of the grid to white ("clears the grid")
 
 const clearButton = document.querySelector("#clear-grid");
 
@@ -19,7 +20,7 @@ clearButton.addEventListener("click", () => {
   });
 });
 
-// make it so when you hover your mouse over the grid, it fills the grid with the color black
+// // make it so when you hover your mouse over the grid, it fills the grid with the color black
 
 gridDiv.addEventListener("mouseover", () => {
   const gridSection = document.querySelector(".grid");
@@ -27,3 +28,11 @@ gridDiv.addEventListener("mouseover", () => {
     gridSection.style.cssText = "background-color: black";
   }
 });
+
+// let isPressed = false;
+// gridContainer.addEventListener("mousedown", () => {isPressed = true;});
+// gridContainer.addEventListener("mouseup", () => {isPressed = true});
+// gridContainer.addEventListener("mousemove", (e) => {isPressed ? e.target.style.backgroundColor = "black" : e.target.backgroundColor = "white"});
+// for(let i = 0; i < 256; i++) {gridContainer.appendChild(document.createElement("div"))}
+
+
